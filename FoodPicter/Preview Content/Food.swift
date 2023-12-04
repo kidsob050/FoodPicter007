2a5b0b6186506fae5e4d003484e44978f772a8c1
kidsob <kidsob7872@gmail.com>
2023-12-04 21:27:28 +0800

    2023/12/04

--- FoodPicter/Preview Content/Food.swift
+++ FoodPicter/Preview Content/Food.swift
@@ -5,4 +5,15 @@
 //  Created by Bernard Bk on 12/3/23.
 //
 
-import Foundation
+struct Food {
+    var name: String
+    var image: String
+    var calorie: Double
+    var carb: Double
+    var fat: Double
+    var protein: Double
+    
+    static let examples = [
+        Food(name: "漢堡", image: "", calorie: 294, carb: 14, fat: 24, protein: 17)
+    ]
+}
